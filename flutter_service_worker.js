5719dd1b4805a504f7d0d4a14020166daf74697d
Jujo-95 <jjgu95@gmail.com>
2023-05-27 14:12:30 -0500

    reports view
    first report dev

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -533,9 +533,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d33249daed208d477c993b22b489c42e",
-"/": "d33249daed208d477c993b22b489c42e",
-"main.dart.js": "99234950a3de3484013d0634f60f2ba5",
+"index.html": "9def367f204162e320f9236d1f3e49c5",
+"/": "9def367f204162e320f9236d1f3e49c5",
+"main.dart.js": "fe358e1a1a5a96eab19c9b4ff3d62d80",
 "manifest.json": "743fdf655fa9b54d071ca4678e2b7be1",
 "version.json": "6d7cd93a5cb0a5e5c032fb3c84c0872c"
 };
