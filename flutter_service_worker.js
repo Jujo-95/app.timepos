cc363c38956cd0dbbaa047349cc60e5ed5087d24
Jujo-95 <jjgu95@gmail.com>
2023-05-29 12:40:43 -0500

    resources tab created
    feature to create resources, like equipment

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -533,9 +533,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9def367f204162e320f9236d1f3e49c5",
-"/": "9def367f204162e320f9236d1f3e49c5",
-"main.dart.js": "fe358e1a1a5a96eab19c9b4ff3d62d80",
+"index.html": "cd5872f67d948d55361f88e50b15dff3",
+"/": "cd5872f67d948d55361f88e50b15dff3",
+"main.dart.js": "a38995e9ab52e616841a9bb733ee9dd7",
 "manifest.json": "743fdf655fa9b54d071ca4678e2b7be1",
 "version.json": "6d7cd93a5cb0a5e5c032fb3c84c0872c"
 };
